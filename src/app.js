898761601b0c8e3c31a2ab6d5fc25ae7ec6fd04e
Tatiana Stewart <tstwrt7@gmail.com>
2024-02-21 00:45:25 -0500

    added GET request

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
+app.get("/restaurants/:id", async (req, res) => {
+    const num = req.params.id;
+    const selectRestaurant = await Restaurant.findByPk(num);
+    res.json(selectRestaurant);
+})
 
 
 
